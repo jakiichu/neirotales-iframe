0c486e26aca183ac196987fa562ed40a4f6dff66
jakiichu4 <zellda444@gmail.com>
2024-12-17 17:53:11 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = (): ReactNode => {
     return (
         <div>
             <iframe width={420} height={1200}
-                    src='https://iframe.kion.neurotales.ipst-dev.com/mainGeneration?iframe=true&user_id=12311456'></iframe>
+                    src='https://iframe.kion.neurotales.dev.ipst-dev.com/mainGeneration?iframe=true&user_id=12311456'></iframe>
         </div>
     );
 };
