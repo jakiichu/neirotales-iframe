acb12bd83e3422164b92c26b7793fa0f921ae262
jakiichu4 <zellda444@gmail.com>
2024-12-17 13:46:48 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {ReactNode} from 'react';
+
+const App = (): ReactNode => {
+    return (
+        <div>
+            <iframe width={420} height={1200}
+                    src='https://iframe.kion.neurotales.ipst-dev.com/mainGeneration?iframe=true&user_id=12311456'></iframe>
+        </div>
+    );
+};
+
+export default App;
