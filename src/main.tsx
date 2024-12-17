1c0cbe5cf16063ff798b23dc2d16e6e0f84915a1
jakiichu4 <zellda444@gmail.com>
2024-12-17 14:35:09 +0700

    init

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import 'eruda';
-if (window.eruda)
-window.eruda.init();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
