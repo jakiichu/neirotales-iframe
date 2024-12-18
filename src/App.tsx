09fe77b33c6f06bc8b05171bea1f9192852de3f7
jakiichu4 <zellda444@gmail.com>
2024-12-18 15:46:38 +0700

    🐹Major.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import {ReactNode} from 'react';
+import {ReactNode, useRef} from 'react';
 
 const App = (): ReactNode => {
+    const ref = useRef(crypto.randomUUID());
     return (
         <div>
             <iframe width={420} height={1200}
-                    src={`https://iframe.kion.neurotales.dev.ipst-dev.com/mainGeneration?iframe=true&user_id=${crypto.randomUUID()}`}></iframe>
+                    src={`https://iframe.kion.neurotales.ipst-dev.com/mainGeneration?iframe=true&user_id=${ref.current}`}></iframe>
         </div>
     );
 };
