f5b472207b3697722cc7477ef25faa07372df9ac
jakiichu <zeLLda444@gmail.com>
2025-04-14 14:58:34 +0700

    asd

--- src/constant/route/index.ts
+++ src/constant/route/index.ts
@@ -1,5 +1,5 @@
 const PROD_FRONT = 'https://iframe.kion.neurotales.ipst-dev.com'
-const DEV_FRONT = 'https://iframe.kion.neurotales.ipst-dev.com'
+const DEV_FRONT = 'https://iframe.kion.neurotales.dev.ipst-dev.com'
 
 export {
     PROD_FRONT,
