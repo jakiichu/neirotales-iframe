f02c719d6398da36b954b92c0cd4eb84f27a20af
jakiichu4 <zellda444@gmail.com>
2025-05-14 15:34:22 +0700

    init

--- src/iframe/index.tsx
+++ src/iframe/index.tsx
@@ -2,26 +2,30 @@ import {memo, ReactNode, useRef} from 'react';
 import {PROD_FRONT} from "../constant/route";
 import getParamsHook from "../utils/params";
 
-interface IIframeItem{
+interface IIframeItem {
     url?: string
 }
-interface IPageParams{
+
+interface IPageParams {
     sandbox?: string
     width?: string
     height?: string
 }
+
 const IframeItem = memo(({url = PROD_FRONT}: IIframeItem): ReactNode => {
     const ref = useRef(crypto.randomUUID());
-    const {sandbox, height=420, width=800} = getParamsHook<IPageParams>()
+    const {sandbox, height = 420, width = 800} = getParamsHook<IPageParams>()
 
-   const params =  new URLSearchParams({
+    const params = new URLSearchParams({
         iframe: "true",
         user_id: ref.current,
         theme: "dark"
     })
+
     return (
-        <div className='flex justify-center py-8 *:rounded-2xl min-h-screen h-screen grow '>
-            <iframe className='h-full min-h-full' sandbox={sandbox} width={Number(width)} height={Number(height)}
+        <div className='flex justify-center py-8 h-screen'>
+            <iframe className='h-full min-h-full rounded-2xl' sandbox={sandbox} width={Number(width)}
+                    height={Number(height)}
                     src={`${url}/mainGeneration?${params.toString()}`}/>
         </div>
     );
